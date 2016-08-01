df3425dbc9fac7c325cf33d5ef50e908ea60ca58
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-01 21:39:21 +0100

    fix typo

--- no_test.sh
+++ no_test.sh
@@ -1,2 +1,2 @@
-java -jar jar/YaraParser.jar parse_partial -input ../data/test.dep  -out ../data/no_model.dep  -model ../data/model/no_model  -punc punc_files/wsj.puncs
+java -jar jar/YaraParser.jar parse_partial -input ../data/test.dep  -out ../data/no_model.dep  -model ../data/model/no_model_best  -punc punc_files/wsj.puncs
 java -jar jar/YaraParser.jar eval -gold ../data/test.dep -parse ../data/no_model.dep -punc punc_files/wsj.puncs
