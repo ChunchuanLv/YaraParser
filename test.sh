3e642cf822b7b3cd24338dc60a4b146d4ad57539
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-02 13:41:50 +0100

    fix parse arguement

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-java -jar jar/YaraParser.jar parse_partial -input ../data/test.dep  -out ../data/vecModel.dep  -model ../data/model/vecModel_best  -punc punc_files/wsj.puncs
+java -jar jar/YaraParser.jar parse_conll -input ../data/test.dep  -out ../data/vecModel.dep  -model ../data/model/vecModel_best  -punc punc_files/wsj.puncs
 java -jar jar/YaraParser.jar eval -gold ../data/test.dep -parse ../data/vecModel.dep -punc punc_files/wsj.puncs
 
