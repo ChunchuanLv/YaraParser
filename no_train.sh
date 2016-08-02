2aeac1027d0cc2a019bb4766baebcb11d6b07432
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-02 13:47:49 +0100

    no train

--- no_train.sh
+++ no_train.sh
@@ -1,4 +1,4 @@
 java -jar  jar/YaraParser.jar train -train-file ../data/train.dep -dev ../data/dev.dep  -model ../data/model/no_model iter:20 -punc punc_files/wsj.puncs
-java -jar jar/YaraParser.jar parse_partial -input ../data/test.dep  -out ../data/no_model.dep  -model ../data/model/no_model_best  -punc punc_files/wsj.puncs
+java -jar jar/YaraParser.jar parse_conll -input ../data/test.dep  -out ../data/no_model.dep  -model ../data/model/no_model_best  -punc punc_files/wsj.puncs
 java -jar jar/YaraParser.jar eval -gold ../data/test.dep -parse ../data/no_model.dep -punc punc_files/wsj.puncs
 
