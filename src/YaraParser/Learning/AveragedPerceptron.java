e17fe7c75686902d637ae48630496eb6af184bb3
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-06 23:25:01 +0100

    _

--- src/YaraParser/Learning/AveragedPerceptron.java
+++ src/YaraParser/Learning/AveragedPerceptron.java
@@ -124,7 +124,6 @@ public class AveragedPerceptron {
 		labelRep =maps.getLabelRep();
 		depMat = labelRep == null ||labelRep.size() >0;
 		dep = (featSize == 27) || (featSize == 73) || (featSize == 154);
-		wdp = new HashMap<Integer,Float>();
 	}
 
 	private HashMap<Integer, Matrix> wordRep;
@@ -145,7 +144,6 @@ public class AveragedPerceptron {
 		contRep = maps.getContRep();
 		labelRep =maps.getLabelRep();
 		depMat = labelRep == null ||labelRep.size() >0;
-		wdp = new HashMap<Integer,Float>();
 	}
 
 	public float changeWeight(Actions actionType, int slotNum, Object featureName, int labelIndex, float change) {
@@ -360,7 +358,6 @@ public float getVecCost(final Object[] features,HashMap<Object, CompactArray>[] 
 		return result ;
 	}
 
-	private HashMap<Integer,Float> wdp ;
 	private float getCostDep(int word, int head, int dep) {
 
 		if (!wordRep.containsKey(word)||!contRep.containsKey(head)||!labelRep.containsKey(dep)) {
