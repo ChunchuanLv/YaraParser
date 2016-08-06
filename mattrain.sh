9400c366a6669682065b4e682326c14a74ebc176
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-06 19:33:48 +0100

    readDep

--- mattrain.sh
+++ mattrain.sh
@@ -1,4 +1,4 @@
-java -jar  jar/YaraParser.jar train nt:16 -train-file ../data/train.dep -dev ../data/dev.dep  -model ..data/model/matModel iter:20 -punc punc_files/wsj.puncs -depe depsF.dep -repPath ../data  depMat
-java -jar jar/YaraParser.jar parse_partial nt:16 -input ../data/test.dep  -out ../data/matModel.dep  -model ../data/model/matModel_best  -punc punc_files/wsj.puncs
-java -jar jar/YaraParser.jar eval nt:16 -gold ../data/test.dep -parse ../data/matModel.dep -punc punc_files/wsj.puncs
+java -jar  jar/YaraParser.jar train  -train-file ../data/train.dep -dev ../data/dev.dep  -model ..data/model/matModel iter:20 -punc punc_files/wsj.puncs -depe depsF.dep -repPath ../data  depMat
+java -jar jar/YaraParser.jar parse_partial  -input ../data/test.dep  -out ../data/matModel.dep  -model ../data/model/matModel_best  -punc punc_files/wsj.puncs
+java -jar jar/YaraParser.jar eval  -gold ../data/test.dep -parse ../data/matModel.dep -punc punc_files/wsj.puncs
 
