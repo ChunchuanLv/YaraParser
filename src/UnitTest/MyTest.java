38b0878aea0c52096ee7b7ca44e12f01413dc98b
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-07 21:18:14 +0100

    1.7

--- src/UnitTest/MyTest.java
+++ src/UnitTest/MyTest.java
@@ -27,12 +27,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import Jama.Matrix;
+import org.jblas.*;
+
 public class MyTest {
 
 	public static void main(String[] args) throws Exception {
 		String s = " 0 1";
-		String[] sv =s.split("[\t ]");
-		for (int i=0;i<sv.length;i++) System.out.println(""+i+":"+sv[i]);
+		String[] sv = s.split("[\t ]");
+		for (int i = 0; i < sv.length; i++)
+			System.out.println("" + i + ":" + sv[i]);
 		Options options = new Options();
 		options.inputFile = "../data/train.dep";
 		options.devPath = "../data/dev.dep  ";
@@ -46,42 +50,86 @@ public class MyTest {
 		options.labeled = true;
 		options.useMaxViol = false;
 		options.numOfThreads = 2;
-		//options.useExtendedFeatures = false;
+		// options.useExtendedFeatures = false;
 
 		// ArrayList<Options> optionList =
 		// Options.getAllPossibleOptions(options);
 		// options.numOfThreads = 2;
 		// for (Options o : optionList)
-	//	testOption(options);
- int z = 66;
- int y = 306446;
- int x = 30634346;
- int iny = y;
- ByteBuffer key = hash(x,y,z);
-	
-	
+		// testOption(options);
+		int length = 10000;
+		int size = 300;
+		double[][] v1 = new double[1][size];
+		double[][] m = new double[size][size];
+		double[][] v2 = new double[size][1];
+		for (int i = 0; i < size; i++) {
+			v1[0][i] = i / 100;
+			v2[i][0] = i / 100;
+			for (int j = 0; j < size; j++)
+				m[i][j] = i + j / 100;
+		}
+		FloatMatrix bm = new DoubleMatrix(m).toFloat();
+		FloatMatrix b1 = new DoubleMatrix(v1).toFloat();
+		FloatMatrix b2 = new DoubleMatrix(v2).toFloat();
+
+		Matrix jm = new Matrix(m);
+		Matrix j1 = new Matrix(v1);
+		Matrix j2 = new Matrix(v2);
+
+		final long startTime = System.currentTimeMillis();
+		float k=0f;
+		for (int i = 0; i < length; i++) {
+			// float k =bm.mulRowVector(b1).mulColumnVector(b2).get(0);
+	k	 = b1.mmul(bm).mmul(b2).get(0);
+		}
+		final long endTime = System.currentTimeMillis();
+
+		System.out.println("Total execution time jblas: " + (endTime - startTime)+" "+k);
+
+		final long startTime1 = System.currentTimeMillis();
+		for (int i = 0; i < length; i++) {
+			 k = 0f;
+			 for (int j =0;j<size;j++)
+				 for (int l =0;l<size;l++)
+					 k
+		}
+		final long endTime1 = System.currentTimeMillis();
+
+		System.out.println("Total execution time jmax: " + (endTime1 - startTime1)+" "+k);
+
+		
+		final long startTime2 = System.currentTimeMillis();
+		for (int i = 0; i < length; i++) {
+			 k = (float) j1.times(jm).times(j2).get(0, 0);
+		}
+		final long endTime2 = System.currentTimeMillis();
+
+		System.out.println("Total execution time jmax: " + (endTime2 - startTime2)+" "+k);
+
+		
 		System.exit(0);
 	}
-private static ByteBuffer hash(int x,int y,int z) {
 
-	ByteBuffer key =  ByteBuffer.allocate(10);
-	key.putInt(x);
-	key.putInt(y);
-	key.putShort((short) z);
-	
+	private static ByteBuffer hash(int x, int y, int z) {
 
-	System.out.println("this key:"+key);
-	System.out.println("this x:"+key.getInt(0));
-	System.out.println("this y:"+key.getInt(4));
-	System.out.println("this z:"+key.getShort(8));
-	return key;
-}
+		ByteBuffer key = ByteBuffer.allocate(10);
+		key.putInt(x);
+		key.putInt(y);
+		key.putShort((short) z);
+
+		System.out.println("this key:" + key);
+		System.out.println("this x:" + key.getInt(0));
+		System.out.println("this y:" + key.getInt(4));
+		System.out.println("this z:" + key.getShort(8));
+		return key;
+	}
+
 	public static void testOption(Options options) throws Exception {
 		System.out.println("**********************************************");
 		System.out.print(options);
 		System.out.println("**********************************************");
 		IndexMaps maps = CoNLLReader.createIndices(options.inputFile, options.labeled, options.lowercase,
-				options.clusterFile, options.repPath, options.depMat,options.we,options.ce,options.depe);
+				options.clusterFile, options.repPath, options.depMat, options.we, options.ce, options.depe);
 		CoNLLReader reader = new CoNLLReader(options.inputFile);
 		ArrayList<GoldConfiguration> dataSet = reader.readData(Integer.MAX_VALUE, false, options.labeled,
 				options.rootFirst, options.lowercase, maps);
@@ -130,17 +178,17 @@ private static ByteBuffer hash(int x,int y,int z) {
 		Configuration configuration = new Configuration(sentence, options.rootFirst, true, false);
 		State currentState = configuration.state;
 		ArcEager.shift(currentState);
-		int stackWordId = sentence.getWords()[currentState.peek()-1];
-		int bufferWordId = sentence.getWords()[ currentState.bufferHead()-1];
+		int stackWordId = sentence.getWords()[currentState.peek() - 1];
+		int bufferWordId = sentence.getWords()[currentState.bufferHead() - 1];
 		Object[] features = FeatureExtractor.extractAllParseFeatures(configuration, featureLength);
 		int bufferF = (int) (long) features[4] - 2;
 		int stackF = (int) (long) features[1] - 2;
-		System.out.println("real stack buffer "+stackWordId+" "+bufferWordId);
-		System.out.println("pred stack buffer "+stackF+" "+bufferF);
+		System.out.println("real stack buffer " + stackWordId + " " + bufferWordId);
+		System.out.println("pred stack buffer " + stackF + " " + bufferF);
 		String out = "";
-		for (int i=0;i<features.length/2-1;i++)
-			out += ", "+(int) (long) features[i];
-		System.out.println(out+"\n");
+		for (int i = 0; i < features.length / 2 - 1; i++)
+			out += ", " + (int) (long) features[i];
+		System.out.println(out + "\n");
 
 	}
 
