041e58485a9b9af6c90ef13e9ed1462d0ae23751
chunchuanlv <chunchuan.lv@gmail.com>
2016-08-12 18:14:35 +0100

    normal

--- mattrain2.sh
+++ mattrain2.sh
@@ -1,4 +1,4 @@
 java -jar  jar/YaraParser.jar train nt:16 -test ../data/test.dep -train-file ../data/train.dep -dev ../data/dev.dep  -model ../data/matModel iter:20 -punc punc_files/wsj.puncs -ce deps.words -depe depsF.dep -repPath ../data  depMat
-java -jar jar/YaraParser.jar parse_conll  -input ../data/test.dep  -out ../data/matModel.dep  -model /disk/cohortnas/masters/s1544871/matModel_best  -punc punc_files/wsj.puncs
+java -jar jar/YaraParser.jar parse_conll  -input ../data/test.dep  -out ../data/matModel.dep  -model ../data/model/matModel_best  -punc punc_files/wsj.puncs
 java -jar jar/YaraParser.jar eval  -gold ../data/test.dep -parse ../data/matModel.dep -punc punc_files/wsj.puncs
 
